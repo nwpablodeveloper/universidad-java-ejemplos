8e45e13021fae22a9ad6d56b7ca203a0b898e3df
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-11 21:09:09 +0100

    seccion 3 - Inferencia de datos

---
+++ seccion3/InferenciaDeDatos/src/inferenciadedatos/InferenciaDeDatos.java
@@ -0,0 +1,11 @@
+package inferenciadedatos;
+
+public class InferenciaDeDatos {
+    public static void main(String[] args) {
+        
+        // var va a hacer inferencia de tipos en Java
+        var miVariableEntera = 10;
+        
+    }
+    
+}
