75437f9d779a84116af0faef07b8103b34b0c47e
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-11 21:34:09 +0100

    seccion 3 - V10 - Reglas para definir una variable en Java

--- seccion3/V10-Reglas-para-definir-una-variable-en-java/src/v10/reglas/para/definir/una/variable/en/java/V10ReglasParaDefinirUnaVariableEnJava.java
+++ seccion3/V10-Reglas-para-definir-una-variable-en-java/src/v10/reglas/para/definir/una/variable/en/java/V10ReglasParaDefinirUnaVariableEnJava.java
@@ -2,5 +2,6 @@ package v10.reglas.para.definir.una.variable.en.java;
 
 public class V10ReglasParaDefinirUnaVariableEnJava {
     public static void main(String[] args) {
+        // lñkjasdflñkasjdf
     }
 }
