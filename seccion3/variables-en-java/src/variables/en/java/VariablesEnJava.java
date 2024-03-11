a5f62e954a7e1dd2ebea9f602832460b8681fb56
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-11 21:10:54 +0100

    seccion 3 - Variables en Java

--- seccion3/variables-en-java/src/variables/en/java/VariablesEnJava.java
+++ seccion3/variables-en-java/src/variables/en/java/VariablesEnJava.java
@@ -1,9 +1,28 @@
 package variables.en.java;
 
 public class VariablesEnJava {
+    
+    // El metodo main nos va a permitir ejecutar nustro programa
     public static void main(String[] args) {
        
-        int miVariableEntera;
-            
+        // Recomendamos notación CamelCase
+        // Usamos el operador de asignación para darle un valor
+        int miVariableEntera = 10;
+        
+        // Imprimir variable
+        System.out.println("mi Variable Entera = " + miVariableEntera);
+        
+        // Podemos sobreescribir en la misma variable
+        miVariableEntera = 20;
+        System.out.println("miVariableEntera = " + miVariableEntera);
+        
+        
+        // VARIABLES DE TIPO STRING
+        
+        String miVariableCadena = "Hola";
+        System.out.println("miVariableCadena = " + miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println("miVariableCadena = " + miVariableCadena);
     }
 }
