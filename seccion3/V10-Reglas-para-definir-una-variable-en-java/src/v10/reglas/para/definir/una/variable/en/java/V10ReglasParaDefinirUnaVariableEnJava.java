65af5c8ac94af9a1a447cd797b859b7de7b73d2d
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-12 20:49:53 +0100

    seccion 3 - V10 - Reglas para definir una variable en Java

--- seccion3/V10-Reglas-para-definir-una-variable-en-java/src/v10/reglas/para/definir/una/variable/en/java/V10ReglasParaDefinirUnaVariableEnJava.java
+++ seccion3/V10-Reglas-para-definir-una-variable-en-java/src/v10/reglas/para/definir/una/variable/en/java/V10ReglasParaDefinirUnaVariableEnJava.java
@@ -2,6 +2,20 @@ package v10.reglas.para.definir.una.variable.en.java;
 
 public class V10ReglasParaDefinirUnaVariableEnJava {
     public static void main(String[] args) {
-        // lñkjasdflñkasjdf
+        
+        // caracteres permitidos al principio de la variable
+        // _ $ 
+        
+        var miVariable = 1;
+        var _miVariable = 2;
+        var $miVaraible = 3;
+        
+        // NO SE RECOMIENDA letras con acentos
+        // ñ Ñ á é í ó ú
+        
+        // No permitido
+        // !"#%&/()
+        
+        
     }
 }
