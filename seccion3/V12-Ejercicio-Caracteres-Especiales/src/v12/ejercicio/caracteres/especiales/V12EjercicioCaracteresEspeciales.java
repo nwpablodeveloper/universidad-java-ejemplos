da20c77175294247d05f61da97460b058c24ea8a
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-12 21:26:00 +0100

    seccion 3 - V12 - Caracteres especiales - Caracter de escape

---
+++ seccion3/V12-Ejercicio-Caracteres-Especiales/src/v12/ejercicio/caracteres/especiales/V12EjercicioCaracteresEspeciales.java
@@ -0,0 +1,29 @@
+package v12.ejercicio.caracteres.especiales;
+
+public class V12EjercicioCaracteresEspeciales {
+
+    public static void main(String[] args) {
+
+        var nombre = "Pablo";
+        
+        // Usamos el caracter de escape alt+92 \
+
+        // Caracter de salto de linea
+        System.out.println("Nueva Linea: \n " + nombre);
+
+        // Caracter de Tabulador
+        System.out.println("Tabulador: \t " + nombre);
+
+        // Carecter de retroceso ( en este ejemplo esta borranod el espacio)
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Retroceder 2 lugares: \b\b " + nombre);
+
+        // Comillas simples no necesita el caracter de escape
+        System.out.println("comilla simplre: \'" + nombre + "\'");
+        System.out.println("comilla simplre: '" + nombre + "'");
+        
+        // Comillas dobles
+        System.out.println("Comillas dobles: \"" + nombre + "\"");
+        
+    }
+}
