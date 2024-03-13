95494617a7524b63802161dcb7e924a056062339
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-13 12:18:52 +0100

    Updates Readme pdfs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### PDFs
 
-## [Seccion 03](PDFs/seccion03/CFJ-A-Leccion-Variables.pdf)
+## Seccion 03
+## [CFJ A Lección de Variables](PDFs/seccion03/CFJ-A-Leccion-Variables.pdf)
+
+## Seccion 04
+## [Variables en Java](PDFs/seccion04/01-VariablesJava.pdf)
--- seccion04/V15-Tipos-Primitivos/src/v15/tipos/primitivos/V15TiposPrimitivos.java
+++ seccion04/V15-Tipos-Primitivos/src/v15/tipos/primitivos/V15TiposPrimitivos.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package v15.tipos.primitivos;
 
-/**
- *
- * @author Pablo
- */
 public class V15TiposPrimitivos {
 
     /**
