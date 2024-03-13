0e861836e23f3220a5b9b5af8ae81e275e6c32e9
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-13 12:40:10 +0100

    seccion 04 - V15 - Tipos Primitivos - Byte

---
+++ seccion04/V15-Tipos-Primitivos - Byte/src/v15/tipos/primitivos/V15TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package v15.tipos.primitivos;
+
+public class V15TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        /*
+            Tipo primitivos de tipos enteros
+            byte, short, int, long
+         */
+        
+        byte numeroByte = 10; // sigue siendo un tipo entero
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("Valor minimo del byte = " + Byte.MIN_VALUE);
+        System.out.println("Valor minimo del byte = " + Byte.MAX_VALUE);
+        // Convertir de entero a tipo byte
+        System.out.println("Convertir 129 a byte");
+        numeroByte = (byte)129;
+        System.out.println("numeroByte2 = " + numeroByte);
+    }
+
+}
