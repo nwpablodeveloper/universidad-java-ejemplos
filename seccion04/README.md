90db0f40060b4d4d6c017da6612173617175cc6b
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-14 17:49:46 +0100

    Update readme sección 04

--- seccion04/README.md
+++ seccion04/README.md
@@ -6,8 +6,9 @@
 * byte 
  - MIN Short: -128
  - MAX Short: 127
+
 ```java
-    // Forzar un numero a ser Byta fuera de su rango
+    // Forzar un numero a ser Byta si estamos fuera de su tamaño
     byte numeroByte = (byte)128;
     // resultado: -127 generando una perdida de precisión
 ```
@@ -17,6 +18,22 @@
  - MIN Short: -32768
  - MAX Short: 32767
 
-[V16 Tipos enteros Numericos - int](V16-Tipos-Enteros-Numericos/src/v16/tipos/enteros/numericos/V16TiposEnterosNumericos.java)
 * int
+ - MIN int: -2147483648
+ - MAX int: 2147483647
 
+```java
+    // Forzar un numero a ser de tipo int si estasmo fuera de su tamaño
+    int numeroInt = (int)2147483647L // Agregamos un L al final
+    // Resultado = -2147483648 generando una perdida de precisión
+```
+
+* Long
+ - MIN long: -9223372036854775808
+ - MAX long: 9223372036854775807
+
+```java
+    // Forzar un numero a ser de tipo long si estamos fuera de su tamaño
+    long numeroLong = (long)9223372036854775808L a long
+    // Resultado: 9223372036854775807 generando una perdida de precisión
+```
--- seccion04/V16-Tipos-Enteros-Numericos/src/v16/tipos/enteros/numericos/V16TiposEnterosNumericos.java
+++ seccion04/V16-Tipos-Enteros-Numericos/src/v16/tipos/enteros/numericos/V16TiposEnterosNumericos.java
@@ -10,8 +10,9 @@ public class V16TiposEnterosNumericos {
         System.out.println("Forzar 32768 a short");
         short numeroShort = (short)32768;
         System.out.println("Forzado = " + numeroShort);
-        System.out.println("Hay perdida de precición");
+        System.out.println("Hay perdida de precisión");
         
+        // Tipo int - puede almacenar hasta 32 bits
         System.out.println("");
         System.out.println("Ej. tipo de dato int:");
         System.out.println("MIN int: " + Integer.MIN_VALUE);
@@ -19,8 +20,9 @@ public class V16TiposEnterosNumericos {
         System.out.println("Forzar 2147483648 a int");
         int numeroInt = (int)2147483648L;
         System.out.println("forzado = " + numeroInt);
-        System.out.println("Hay perdida de precición");
+        System.out.println("Hay perdida de precisión");
         
+        // Tipo Long - puede almacenar hasta 64 bits
         System.out.println("");
         System.out.println("Ej. Tipo de dato long");
         System.out.println("MIN long: " + Long.MIN_VALUE);
@@ -28,6 +30,7 @@ public class V16TiposEnterosNumericos {
         System.out.println("Forzar 9223372036854775808L a long");
         long numeroLong = (long)9223372036854775808F;
         System.out.println("Forzado = " + numeroLong);
+        System.out.println("Hay perdida de precisión");
         
         
     }
