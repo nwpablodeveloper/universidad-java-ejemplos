15ff2905243e06b0501cb0ce94f880f8bd8130fd
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-15 21:19:01 +0100

    V18_Ejercicio_de_Tipos_con_Var_y_tipos_Primitivos

---
+++ seccion04/V18_Ejercicio_de_Tipos_con_Var_y_tipos_Primitivos/src/practica/practica.java
@@ -0,0 +1,28 @@
+package practica;
+
+public class practica {
+    public static void main(String[] args) {
+        
+        // Inferencia del tipo de dato con var
+        
+        // Inferencia de dato de tipo entero
+        var numeroEnteros1 = 8;
+        var numeroEnteros2 = 35;
+        var numeroEnteros3 = 40;
+        var numeroEnteros4 = 80;
+        
+        // Inferencia de dato de tipo double
+        // Por defecto un numero con decimal es de tipo double
+        var numeroDouble1 = 10.3;
+        var numeroDouble2 = 10.3;
+        var numeroDouble3 = 10.3;
+        var numeroDouble4 = 10.3;
+
+        // Inferencia de dato de tipo float
+        // Necesitamos indicar al numero con F
+        var numeroFloat1 = 35.0F;
+        var numeroFloat2 = 355.7F;
+        var numeroFloat3 = 435.654F;
+        var numeroFloat4 = 488535.878F;
+    }
+}
---
+++ seccion04/V18_Ejercicio_de_Tipos_con_Var_y_tipos_Primitivos/src/v18_ejercicio_de_tipos_con_var_y_tipos_primitivos/V18_Ejercicio_de_Tipos_con_Var_y_tipos_Primitivos.java
@@ -0,0 +1,18 @@
+package v18_ejercicio_de_tipos_con_var_y_tipos_primitivos;
+
+public class V18_Ejercicio_de_Tipos_con_Var_y_tipos_Primitivos {
+    public static void main(String[] args) {
+        
+        var numeroEntero = 35;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        System.out.println("");
+        
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = (float)10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+    }
+    
+}
