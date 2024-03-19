2935f1b924e86df1d54bb4ad92de9088ab6dfd08
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-19 23:55:21 +0100

    V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1

--- seccion04/V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1/src/v21_ejercicio_conversion_de_tipos_primitivos_en_java_parte_1/V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1.java
+++ seccion04/V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1/src/v21_ejercicio_conversion_de_tipos_primitivos_en_java_parte_1/V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1.java
@@ -1,7 +1,20 @@
 package v21_ejercicio_conversion_de_tipos_primitivos_en_java_parte_1;
 
+import java.util.Scanner;
+
 public class V21_Ejercicio_Conversion_de_Tipos_Primitivos_en_Java_Parte_1 {
     public static void main(String[] args) {
-        var edad = "20";
+        
+        var edad = Integer.parseInt("20");
+        System.out.println("edad = " + edad);
+        
+        
+        var valorPi = Double.parseDouble("3.1416");
+        System.out.println("valorPi = " + valorPi);
+        
+        var consola = new Scanner(System.in);
+        System.out.println("Indica tu edad");
+        edad = Integer.parseInt(consola.nextLine());
+        
     }
 }
