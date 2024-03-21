2f560028683041db9ab920f7e12479d43cf02c8c
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-21 13:38:38 +0100

    Seccion05

---
+++ seccion05/V24_Ejercicio_Operadores_Aritmeticos_en_Java/src/v24_ejercicio_operadores_aritmeticos_en_java/V24_Ejercicio_Operadores_Aritmeticos_en_Java.java
@@ -0,0 +1,8 @@
+package v24_ejercicio_operadores_aritmeticos_en_java;
+
+public class V24_Ejercicio_Operadores_Aritmeticos_en_Java {
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+        var reultado = a + b;
+    }
+}
