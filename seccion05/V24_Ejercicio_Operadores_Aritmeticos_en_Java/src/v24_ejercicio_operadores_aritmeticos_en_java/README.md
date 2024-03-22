d0bbbb04419301a2c1573c9c57790c83b775e87e
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-22 13:16:05 +0100

    V24_Ejercicio_Operadores_Aritmeticos_en_Java

---
+++ seccion05/V24_Ejercicio_Operadores_Aritmeticos_en_Java/src/v24_ejercicio_operadores_aritmeticos_en_java/README.md
@@ -0,0 +1,3 @@
+<img src="V24.png"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
--- seccion05/V24_Ejercicio_Operadores_Aritmeticos_en_Java/src/v24_ejercicio_operadores_aritmeticos_en_java/V24_Ejercicio_Operadores_Aritmeticos_en_Java.java
+++ seccion05/V24_Ejercicio_Operadores_Aritmeticos_en_Java/src/v24_ejercicio_operadores_aritmeticos_en_java/V24_Ejercicio_Operadores_Aritmeticos_en_Java.java
@@ -2,7 +2,29 @@ package v24_ejercicio_operadores_aritmeticos_en_java;
 
 public class V24_Ejercicio_Operadores_Aritmeticos_en_Java {
     public static void main(String[] args) {
-        int a = 3, b = 2;
-        var reultado = a + b;
+        
+        int a = 15, b = 6;
+        System.out.println( "a = " + a + "\nb = " + b);
+        System.out.println("");
+        
+        var resultado = a + b;
+        System.out.println(resultado + "\t = suma");
+        
+        resultado = a - b;
+        System.out.println(resultado + "\t = resta");
+        
+        resultado = a * b;
+        System.out.println(resultado + "\t = Multiplicar");
+        
+        resultado = a / b;
+        System.out.println(resultado + "\t = División ( solo parte entera )");
+        
+        var resultadoDouble = a / 3D;
+        System.out.println( resultadoDouble+ "\t = División a / 3D");
+        
+        resultado = a % b;
+        System.out.println(resultado + "\t = Resto entero de la división" );        
+        
+        
     }
 }
