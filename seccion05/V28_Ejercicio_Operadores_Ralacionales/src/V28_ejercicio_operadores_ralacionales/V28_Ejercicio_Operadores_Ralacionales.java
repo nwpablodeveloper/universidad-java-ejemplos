2ad737ce60c0d92d609200bf5c7466a4898c9f79
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-25 19:01:28 +0100

    V28_Ejercicio_Operadores_Ralacionales

---
+++ seccion05/V28_Ejercicio_Operadores_Ralacionales/src/V28_ejercicio_operadores_ralacionales/V28_Ejercicio_Operadores_Ralacionales.java
@@ -0,0 +1,15 @@
+package V28_ejercicio_operadores_ralacionales;
+
+public class V28_Ejercicio_Operadores_Ralacionales {
+    public static void main(String[] args) {
+        
+        var a = 3;
+        var b = 2;
+        
+        var g1 = a > b;
+        var g2 = a < b;
+        var g3 = a >= b;
+        var g4 = a <= b;
+        
+    }
+}
