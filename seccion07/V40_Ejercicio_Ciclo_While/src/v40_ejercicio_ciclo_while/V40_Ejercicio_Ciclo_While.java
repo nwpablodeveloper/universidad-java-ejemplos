99ed0ab13399e428c8f29e21d7f2665164c9f02c
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-26 17:43:41 +0100

    V40_Ejercicio_Ciclo_While

---
+++ seccion07/V40_Ejercicio_Ciclo_While/src/v40_ejercicio_ciclo_while/V40_Ejercicio_Ciclo_While.java
@@ -0,0 +1,23 @@
+package v40_ejercicio_ciclo_while;
+
+import java.util.Scanner;
+
+public class V40_Ejercicio_Ciclo_While {
+    public static void main(String[] args) {
+        
+        Scanner inputNum = new Scanner(System.in);
+        
+        System.out.println("Ingese cantidad de repeticiones");
+        int repeticiones = Integer.parseInt(inputNum.nextLine());
+        
+        int contador = 1;
+        while( contador <= repeticiones ){
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+        
+        
+        
+    }
+    
+}
