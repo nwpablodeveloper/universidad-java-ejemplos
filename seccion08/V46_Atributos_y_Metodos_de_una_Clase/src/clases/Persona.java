87b021fd7f242760c3b0234e2d41d7d9d81d4d21
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-28 17:53:11 +0100

    V46_Atributos_y_Metodos_de_una_Clase

---
+++ seccion08/V46_Atributos_y_Metodos_de_una_Clase/src/clases/Persona.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Persona {
+
+    // Definición de los atributos de nuestra clase
+    String nombre;
+    String apellido;
+    
+    
+    // Definición de metodos
+    public void desplegarInformacion(){
+        System.out.println("Nombre" + nombre);
+        System.out.println("Apellido" + apellido);
+    }
+    
+}
