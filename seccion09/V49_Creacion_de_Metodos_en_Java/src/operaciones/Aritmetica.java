f477ba8f5693c5a09535ec97b73b12a480604c81
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-29 00:40:31 +0100

    V49_Creacion_de_Metodos_en_Java

---
+++ seccion09/V49_Creacion_de_Metodos_en_Java/src/operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+package operaciones;
+
+public class Aritmetica {
+    // Atributos de la clase
+    int a;
+    int b;
+    
+    // Métodos    
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
