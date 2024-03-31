dfd0ba639b10ce827277d9229782f8ae8744ccd5
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-03-31 20:20:03 +0200

    Update PasoPorReferencia

--- Fundamentos_Java/seccion12/V60_Ejercicio_Paso_por_Referencia_en_Java/src/pasoporreferencia/PasoPorReferencia.java
+++ Fundamentos_Java/seccion12/V60_Ejercicio_Paso_por_Referencia_en_Java/src/pasoporreferencia/PasoPorReferencia.java
@@ -5,16 +5,19 @@ import clases.Persona;
 public class PasoPorReferencia {
     public static void main(String[] args) {
         
+        // Creamos una variable Object del tipo Persona
         Persona persona1 = new Persona();
         
-        // Los atributos tienen que tener el modificadr PUBLIC para poder ser
-        // accedidos ya que la clase de Persona se encuentra en otro paquete
+        /* 
+            Los atributos tienen que tener el modificadr PUBLIC para poder ser
+            accedidos ya que la clase de Persona se encuentra en otro paquete
+        */ 
         System.out.println("Dclaramos un objeto de tipo Persona llamado persona1");
         persona1.nombre = "Pablo";
         persona1.apellido = "Veiga";
         persona1.deplegarInformación();
         
-        // Pasamos la variable como parametro al metodo
+        // Pasamos la variable de tipo Object como parametro al metodo
         cambiaValor(persona1);
         
         persona1.deplegarInformación();
@@ -23,9 +26,9 @@ public class PasoPorReferencia {
     
     public static void cambiaValor(Persona persona){
         /*
-            Si bien podemos crear una nueva variable dentro de este metodo
-            La copia de la misma sigue haciendo referencia a la enviada por
-            argumento
+            Si bien podemos crear una nueva variable dentro de este metodo,
+            ss algo que no se hace porque sigue haciendo referencia a la 
+            variable de tipo Object enviada por argumento.
         */
         System.out.println("");
         System.out.println("Cambiamos el valor de la variable persona1 ");
