55f8fcfc9eade6b2ffd2abde8e27b0aaac0b769b
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-01 21:23:34 +0200

    V68_Ejercicio_Contexto_Estatico_en_Java_

---
+++ Fundamentos_Java/seccion16/V68_Ejercicio_Contexto_Estatico_en_Java_/src/test/PersonaPrueba.java
@@ -0,0 +1,18 @@
+package test;
+
+import domain.Persona;
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona("Pablo");
+        System.out.println("persona1 = " + persona1);
+        
+        Persona persona2 = new Persona("Romina");
+        System.out.println("persona2 = " + persona2);
+        
+        Persona persona3 = new Persona("Sophia");
+        System.out.println("persona3 = " + persona3);
+        
+    }
+}
