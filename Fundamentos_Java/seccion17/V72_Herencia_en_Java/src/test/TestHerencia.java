ec1078d814c8df992d5f160000e5d406c0a6d379
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-03 17:43:57 +0200

    V72_Herencia_en_Java

---
+++ Fundamentos_Java/seccion17/V72_Herencia_en_Java/src/test/TestHerencia.java
@@ -0,0 +1,14 @@
+package test;
+
+import domain.Empleado;
+import domain.Persona;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+       
+        
+    }
+}
