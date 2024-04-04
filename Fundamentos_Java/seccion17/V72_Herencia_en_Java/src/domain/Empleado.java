fda296f5d2732bef9650011dc6dffaf0703db108
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-04 22:36:24 +0200

    V73_Herencia_en_Java

--- Fundamentos_Java/seccion17/V72_Herencia_en_Java/src/domain/Empleado.java
+++ Fundamentos_Java/seccion17/V72_Herencia_en_Java/src/domain/Empleado.java
@@ -6,5 +6,35 @@ public class Empleado extends Persona{
     private int idEmpleado;
     private static int contadorEmpleados;
     
+    public Empleado(String nombre,  double sueldo){
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{");
+        sb.append("sueldo=").append(sueldo);
+        sb.append(", idEmpleado=").append(idEmpleado);
+        sb.append(", \n ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
     
 }
--- Fundamentos_Java/seccion17/V72_Herencia_en_Java/src/test/TestHerencia.java
+++ Fundamentos_Java/seccion17/V72_Herencia_en_Java/src/test/TestHerencia.java
@@ -6,8 +6,11 @@ import domain.Persona;
 public class TestHerencia {
     public static void main(String[] args) {
         
-        Empleado empleado1 = new Empleado();
+        Empleado empleado1 = new Empleado("Pablo", 1799);
         System.out.println("empleado1 = " + empleado1);
+        
+        Empleado empleado2 = new Empleado("Pablo", 1799);
+        System.out.println("empleado1 = " + empleado2);
        
         
     }
