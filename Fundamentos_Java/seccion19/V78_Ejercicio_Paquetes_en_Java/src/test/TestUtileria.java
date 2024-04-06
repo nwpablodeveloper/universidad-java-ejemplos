5a7ea327a26decde43e4ca1a6760664d988b5d21
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-06 19:15:08 +0200

    V78_Ejercicio_Paquetes_en_Java

---
+++ Fundamentos_Java/seccion19/V78_Ejercicio_Paquetes_en_Java/src/test/TestUtileria.java
@@ -0,0 +1,15 @@
+package test;
+
+// import mx.com.globalmentoring.*;
+// import mx.com.globalmentoring.Utileria;
+// import static mx.com.globalmentoring.Utileria.imprimir;
+import static mx.com.globalmentoring.Utileria.*;
+
+public class TestUtileria {
+    public static void main(String[] args) {
+        
+        imprimir("Desde otro Paquete");
+        mx.com.globalmentoring.Utileria.imprimir("Asi támbien");
+        
+    }
+}
