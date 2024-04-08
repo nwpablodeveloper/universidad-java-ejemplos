e2f47487a145cb3b85858b65d46c8e01819225b4
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-08 12:19:15 +0200

    V81_Arreglos_en_Java

---
+++ Fundamentos_Java/seccion21/V81_Arreglos_en_Java/src/test/TestArreglos.java
@@ -0,0 +1,21 @@
+package test;
+
+public class TestArreglos {
+    public static void main(String[] args) {
+        
+        /*
+            Los arreglos los declaramos como un objeto junto a unos corchetes.
+            Los corchetes pueden ir adelante o al final del nombre de la 
+            variables, se acostumbra al final. 
+        */
+        
+        String []nombres = new String[5];
+        int edades[] = new int[5];
+        
+        System.out.println("edades = " + edades);
+        
+        // Sintaxis resumida
+        String frutas[] = {"Mandarina", "Naranjas", "Frutillas", "uva"};
+        
+    }
+}
