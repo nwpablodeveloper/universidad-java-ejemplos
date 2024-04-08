037768b1a97278e1f24305759ca3d52be216a1b5
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-08 13:11:04 +0200

    V88_Manejos_de_Matrices_parte_2

--- Fundamentos_Java/seccion22/V88_Manejos_de_Matrices_parte_2/src/test/TestMatrices.java
+++ Fundamentos_Java/seccion22/V88_Manejos_de_Matrices_parte_2/src/test/TestMatrices.java
@@ -8,7 +8,24 @@ public class TestMatrices {
             elementos de tipo enteros
         */
         int edades[][] = new int[3][2];
-        System.out.println("edades = " + edades);
+
+        edades[0][0] = 23;
+        edades[0][1] = 4;
         
+        edades[1][0] = 27;
+        edades[1][1] = 17;
+        
+        edades[2][0] = 71;
+        edades[2][1] = 17;
+        
+        System.out.println("edades[0][0] = " + edades[0][0]);
+        System.out.println("edades[0][1] = " + edades[0][1]);
+        
+        System.out.println("edades[1][0] = " + edades[1][0]);
+        System.out.println("edades[1][1] = " + edades[1][1]);
+        
+        System.out.println("edades[2][0] = " + edades[2][0]);
+        System.out.println("edades[2][1] = " + edades[2][1]);
     }
 }
+
