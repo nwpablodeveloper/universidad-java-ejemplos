14b978247273b3dc60477a3de6450597d589ff99
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-10 12:06:15 +0200

    V95_Ejercicio_Disenio_de_Clases_parte_3

---
+++ Fundamentos_Java/seccion23/V95_Ejercicio_Disenio_de_Clases_parte_3/src/ms/com/gm/ventas/Producto.java
@@ -0,0 +1,52 @@
+package ms.com.gm.ventas;
+
+public class Producto {
+    
+    private final int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+    
+    private Producto(){
+        this.idProducto = Producto.contadorProductos++;
+    }
+    
+    public Producto(String nombre, double precio){
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return this.idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{");
+        sb.append("idProducto=").append(idProducto);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+}
