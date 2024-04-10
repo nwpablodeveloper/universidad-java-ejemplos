27a02c0584cfbb77b0e47a009460f4bf56692044
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-10 12:28:06 +0200

    V96_Ejercicio_Disenio_de_Clases_parte_4

---
+++ Fundamentos_Java/seccion23/V96_Ejercicio_Disenio_de_Clases_parte_4/src/ms/com/gm/ventas/Orden.java
@@ -0,0 +1,31 @@
+package ms.com.gm.ventas;
+
+public class Orden {
+
+    private final int idOrden;
+    private Producto productos[];
+    private static int contadorOrdenes;
+    private int contadorProcutos;
+    private static final int MAX_PRODUCTOS = 10;
+
+    public Orden() {
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto) {
+        if (this.contadorProcutos == Orden.MAX_PRODUCTOS) 
+            System.out.println("Superado el limite de prodúctos para la orden");
+        
+        this.productos[this.contadorProcutos++] = producto;
+    }
+
+    public double calcularTotal(){
+        double total = 0;
+        for (int i = 0; i < this.contadorProcutos; i++) {
+            total += this.productos[i].getPrecio();
+        }
+        return total;
+    }
+    
+}
