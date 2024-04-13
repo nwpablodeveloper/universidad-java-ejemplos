bf5afdc0348b570ae5908b5214ecb9c24fdbdcc4
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-13 16:45:57 +0200

    V111_Manejo_de_ForEach

---
+++ T2_Programacion/seccion28/V111_Manejo_de_ForEach/src/test/TestForEach.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        
+        int edades[] = {8, 35, 60, 16};
+        
+        for (int edad : edades) {
+            System.out.println("edad: " + edad);
+        }
+        
+    }
+}
