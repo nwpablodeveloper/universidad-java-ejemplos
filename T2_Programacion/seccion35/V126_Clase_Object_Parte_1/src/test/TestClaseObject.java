2fc5153a00265b823f38ce0e7cd3a2c602c8712f
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-15 17:03:14 +0200

    V126_Clase_Object_Parte_1

--- T2_Programacion/seccion35/V126_Clase_Object_Parte_1/src/test/TestClaseObject.java
+++ T2_Programacion/seccion35/V126_Clase_Object_Parte_1/src/test/TestClaseObject.java
@@ -8,17 +8,16 @@ public class TestClaseObject {
         Empleado empleado1 = new Empleado("Pablo", 1500);
         Empleado empleado2 = new Empleado("Pablo", 1500);
         
+        /*
+            Preguntar si los 2 objetos tienen la misma referencia en
+            memoria
+        */
+        
         if ( empleado1 == empleado2 ) {
             System.out.println("Tienen la misma ref. en memoria");
         }else{
             System.out.println("Tienen Disitinta ref. en memoria");
         }
-        
-        /*
-            Preguntamos por el contenido de los 2 Objetos para saber si
-            son iguales
-        */
-        
         
     }
 }
