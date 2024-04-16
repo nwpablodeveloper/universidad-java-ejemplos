d259dc8c73d91fb081030598f2ddb2007764e90f
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-16 15:26:27 +0200

    V131_Manejo_de_Interfaces_Parte_2

---
+++ T2_Programacion/seccion37/V131_Manejo_de_Interfaces_Parte_2/src/accesodatos/IAccesoDatos.java
@@ -0,0 +1,22 @@
+package accesodatos;
+
+public interface IAccesoDatos {
+    /*
+        Las interfaces dectectan automaticamente a todas las variables
+        como constantes. ( public final static ).
+        Estamos obligados a asignarles un valor.
+    */
+    int MAX_REGISTROS = 10;
+    
+    /*
+        Cualquier métdo que declaremos va a ser de modificador público y 
+        abstracto
+    */
+    
+    // método abstratoc ( ver con Ctrl. y mouse arriba )
+    void insertar();
+    void listar();
+    void actualizar();
+    void eliminar();
+    
+}
