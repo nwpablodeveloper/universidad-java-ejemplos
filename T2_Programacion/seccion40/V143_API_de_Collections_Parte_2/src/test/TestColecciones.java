a7b2bef8bac8adc10d37128cc7b8063464681e60
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-17 21:23:33 +0200

    V143_API_de_Collections_Parte_2

---
+++ T2_Programacion/seccion40/V143_API_de_Collections_Parte_2/src/test/TestColecciones.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestColecciones {
+
+    public static void main(String[] args) {
+
+        List miLista = new ArrayList();
+
+        /*
+            Se puede cargar de forma dinámica y mantiene el orden en el que fue 
+            cargado
+         */
+        miLista.add("martes");
+        miLista.add("lunes");
+        miLista.add("jueves");
+        miLista.add("miercoles");
+
+        // Interar con forEach
+        for (Object elemetno : miLista) {
+            System.out.println("elemetno: " + elemetno);
+        }
+
+        System.out.println("");
+        
+        // Interar con función flecha
+        miLista.forEach(elemento -> {
+            System.out.println("elemento:  " + elemento);
+        }
+    
+
+);
+
+    }
+}
