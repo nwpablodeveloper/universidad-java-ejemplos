852518113bcfa8a8d7f48464fe7d32de6c48b5a4
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-18 19:18:40 +0200

    V150_Manejos_de_Archivos_Parte_1

---
+++ T2_Programacion/seccion42/V150_Manejos_de_Archivos_Parte_1/src/majoarchivos/ManejoArchivos.java
@@ -0,0 +1,26 @@
+package majoarchivos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class ManejoArchivos {
+
+    public static void crearArchivo(String nombreArchivo) {
+        
+        File archivo = new File(nombreArchivo);
+
+        try {
+            // Abrimos un nuevo archivo con el nombre que le indicamos
+            PrintWriter salida = new PrintWriter(archivo);
+            
+            // Cerramos el flujo que se abrio
+            salida.close(); // Aca es donde se crea el archivo en el HDD
+            System.out.println("Archivo creado");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error al guardar el archivo");
+            ex.printStackTrace(System.out);
+        }
+    }
+
+}
