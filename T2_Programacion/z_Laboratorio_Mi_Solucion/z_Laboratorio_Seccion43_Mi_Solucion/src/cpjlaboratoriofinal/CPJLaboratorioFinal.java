0eb7e0c440ad8b043cfc5440d9dd70b5169999b3
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-04-23 20:43:18 +0200

    z_Laboratorio_Seccion43_Mi_Solucion

--- T2_Programacion/z_Laboratorio_Mi_Solucion/z_Laboratorio_Seccion43_Mi_Solucion/src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ T2_Programacion/z_Laboratorio_Mi_Solucion/z_Laboratorio_Seccion43_Mi_Solucion/src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -3,21 +3,27 @@ package cpjlaboratoriofinal;
 import java.util.Scanner;
 
 public class CPJLaboratorioFinal {
+
     public static void main(String[] args) {
-        
+
         Scanner consola = new Scanner(System.in);
-        
+
         String menu = "INGRESE UNA OPCION: ";
         menu += "\n1. Iniciar Catalogo de peliculas";
         menu += "\n2. Agregar Pelicula";
         menu += "\n3. Listar Pelicula";
         menu += "\n4. Buscar Pelicula";
         menu += "\n0. Salir Pelicula";
-        
-        System.out.println(menu );
-        
-        int opcion = Integer.parseInt(consola.nextLine());
-        System.out.println("Su eleccion fue: " + opcion);
-        
+
+        int opcion;
+
+        do {
+            System.out.println(menu);
+
+            opcion = Integer.parseInt(consola.nextLine());
+            System.out.println("Su eleccion fue: " + opcion);
+
+        } while (opcion <= -1 || opcion > 4);
+
     }
 }
