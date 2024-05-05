f64a6278239f52b490f1287e8c0ba661768e8644
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-05 13:31:49 +0200

    V173_Cadena_de_Conexion_a_MySql

---
+++ T3_java_con_jdbc/seccion45/V173_Cadena_de_Conexion_a_MySql/src/main/java/test/TestMySqlJDBC.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TestMySqlJDBC {
+    public static void main(String[] args) {
+        
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        
+    }
+}
