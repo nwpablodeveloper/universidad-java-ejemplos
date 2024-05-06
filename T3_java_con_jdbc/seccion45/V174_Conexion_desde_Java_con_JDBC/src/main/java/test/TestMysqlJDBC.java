78716d0282fa3aec2f16d8a78e8d678449e1cceb
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-06 21:56:10 +0200

    V174_Conexion_desde_Java_con_JDBC

---
+++ T3_java_con_jdbc/seccion45/V174_Conexion_desde_Java_con_JDBC/src/main/java/test/TestMysqlJDBC.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class TestMysqlJDBC {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        
+        try {
+            // requerido para aplicaciones web
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(url, "root", "admin");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT id_persona, nombre, apellido, email, telefono FROM personas";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            System.out.println("resultado = " + resultado);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+}
