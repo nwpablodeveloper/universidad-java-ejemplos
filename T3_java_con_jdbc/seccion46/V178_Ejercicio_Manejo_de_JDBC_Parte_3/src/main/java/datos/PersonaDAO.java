7df13fbcee58f6d546820c64a553321023007874
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-12 23:47:38 +0200

    PersonaDAO

---
+++ T3_java_con_jdbc/seccion46/V178_Ejercicio_Manejo_de_JDBC_Parte_3/src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,14 @@
+package datos;
+
+import domain.Persona;
+import java.util.List;
+
+public class PersonaDAO {
+    
+    private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM test.personas";
+    
+    public List<Persona> seleccionar(){
+        return 
+    } 
+    
+}
