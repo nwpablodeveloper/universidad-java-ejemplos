413b653bf594b93ab93b78e3d77828e106d91719
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-17 12:35:02 +0200

    Explicación tarea hacer el método editar y eliminar

--- T3_java_con_jdbc/seccion46/V182_Ejercicio_Manejo_de_JDBC_Parte_7/src/main/java/datos/PersonaDAO.java
+++ T3_java_con_jdbc/seccion46/V182_Ejercicio_Manejo_de_JDBC_Parte_7/src/main/java/datos/PersonaDAO.java
@@ -4,60 +4,42 @@ import static datos.Conexion.*;
 import domain.Persona;
 import java.sql.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PersonaDAO {
-
-    private static final String SQL_SELECT = "SELECT "
-            + "id_persona, "
-            + "nombre, "
-            + "apellido, "
-            + "email, "
-            + "telefono "
-            + "FROM test.personas";
-
-    private static final String SQL_INSERT = "INSERT INTO personas(nombre, apellido, email, telefono) "
-            + "VALUES(?,?,?,?)";
-
-    private static final String SQL_EDITAR = "UPDATE personas SET "
-            + "nombre = ?, "
-            + "apellido = ?, "
-            + "email = ?, "
-            + "telefono = ? "
-            + "WHERE id_persona = ?";
-
-    private static final String SQL_ELIMINAR = "DELETE FROM persnas WEHRE id_persona = ?";
-
-    public List<Persona> seleccionar() {
-
+    
+    private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM test.personas";
+    private static final String SQL_INSERT = "INSERT INTO personas(nombre, apellido, email, telefono) VALUES(?,?,?,?)";
+    
+    public List<Persona> seleccionar(){
+        
         List<Persona> personas = new ArrayList<>();
-
+        
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         Persona persona = null;
-
+        
+        
         try {
             conn = getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             rs = stmt.executeQuery();
-
-            while (rs.next()) {
+            
+            while( rs.next()){
                 int idPersona = rs.getInt("id_persona");
                 String nombre = rs.getString("nombre");
                 String apellido = rs.getString("apellido");
                 String email = rs.getString("email");
-                String telefono = rs.getString("telefono");
-
+                String telefono= rs.getString("telefono");
+                
                 persona = new Persona(idPersona, nombre, apellido, email, telefono);
-
+                
                 personas.add(persona);
             }
-
+            
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        } finally {
+        } finally{
             try {
                 close(rs);
                 close(stmt);
@@ -66,29 +48,29 @@ public class PersonaDAO {
                 ex.printStackTrace(System.out);
             }
         }
-
+        
         return personas;
-    }
-
-    public int insertar(Persona persona) {
+    } 
+    
+    public int insertar(Persona persona){
         Connection conn = null;
         PreparedStatement stmt = null;
         int registros = 0;
-
+        
         try {
             conn = getConnection();
             stmt = conn.prepareStatement(SQL_INSERT);
-
+            
             // Cada nro corresponde a la posición del signo de pregunta que esta en la query
             stmt.setString(1, persona.getNombre());
             stmt.setString(2, persona.getApellido());
             stmt.setString(3, persona.getEmail());
             stmt.setString(4, persona.getTelefono());
             registros = stmt.executeUpdate();
-
+            
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        } finally {
+        } finally{
             try {
                 close(stmt);
                 close(conn);
@@ -96,63 +78,9 @@ public class PersonaDAO {
                 ex.printStackTrace(System.out);
             }
         }
-
+        
+        
         return registros;
     }
-
-    public Persona editar(Persona persona) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        int registros = 0;
-
-        try {
-            conn = getConnection();
-            stmt = conn.prepareStatement(SQL_EDITAR);
-
-            stmt.setString(1, persona.getNombre());
-            stmt.setString(2, persona.getApellido());
-            stmt.setString(3, persona.getEmail());
-            stmt.setString(4, persona.getTelefono());
-            stmt.setInt(5, persona.getIdPersona());
-            registros = stmt.executeUpdate();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
-        } finally {
-            try {
-                close(stmt);
-                close(stmt);
-            } catch (SQLException ex) {
-                ex.printStackTrace(System.out);
-            }
-        }
-
-        return persona;
-    }
     
-    public void eliminar(int id_persona){
-        
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        
-        try {
-            
-            conn = getConnection();
-            stmt = conn.prepareStatement(SQL_ELIMINAR);
-            stmt.setInt(1, id_persona);
-            stmt.executeUpdate();
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
-        }finally{
-            try {
-                close(stmt);
-                close(conn);
-            } catch (SQLException ex) {
-                ex.printStackTrace(System.out);
-            }
-        }
-        
-    }
 }
--- T3_java_con_jdbc/seccion46/V182_Ejercicio_Manejo_de_JDBC_Parte_7/src/main/java/test/TestManejoPersonas.java
+++ T3_java_con_jdbc/seccion46/V182_Ejercicio_Manejo_de_JDBC_Parte_7/src/main/java/test/TestManejoPersonas.java
@@ -5,42 +5,20 @@ import domain.Persona;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class TestManejoPersonas {
-
     public static void main(String[] args) {
-
+    
         PersonaDAO personaDAO = new PersonaDAO();
-
+        
         // Insertando un nuevo objeto en la DB
-        
-        personaDAO.insertar(
-                new Persona(
-                        "pablo", 
-                        "veiga", 
-                        "veiga@gmail.com", 
-                        "613826756"));
-        
-        for (int i = 0; i < 100; i++) {
-
-            personaDAO.insertar(
-                    new Persona(
-                            "pablo " + i,
-                            "veiga " + i,
-                            "veiga" + i + "@gmail.com",
-                            "613826756 " + i));
-        }
-
-        // Editar registro
-        personaDAO.editar(new Persona(3, "romina", "diaz", "romi@gmail.com", "654654"));
-
-        // Eliminar registro
-        personaDAO.eliminar(55);
+        personaDAO.insertar(new Persona("pablo", "veiga", "veiga@gmail.com", "613826756"));
         
         List<Persona> personas = personaDAO.seleccionar();
-
-        personas.forEach(persona -> {
+        
+        personas.forEach( persona -> {
             System.out.println(persona);
         });
-
+        
     }
 }
