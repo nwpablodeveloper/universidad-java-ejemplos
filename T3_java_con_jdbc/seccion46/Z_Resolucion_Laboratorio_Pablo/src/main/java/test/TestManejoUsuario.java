424d6e57ad274a238ba8fe26cba1a4ae0fa06e45
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-21 00:28:14 +0200

    Main Manejo de Usuarios

--- T3_java_con_jdbc/seccion46/Z_Resolucion_Laboratorio_Pablo/src/main/java/test/TestManejoUsuario.java
+++ T3_java_con_jdbc/seccion46/Z_Resolucion_Laboratorio_Pablo/src/main/java/test/TestManejoUsuario.java
@@ -10,7 +10,7 @@ public class TestManejoUsuario {
 
         UsuarioDAO usuarioDAO = new UsuarioDAO();
 
-        //usuarioDAO.insertarUsuario(new Usuario("nwpablo", "123456"));
+        usuarioDAO.insertarUsuario(new Usuario("nwpablo", "123456"));
         usuarioDAO.editarUsuario(new Usuario(4, "Sophi", "2015"));
         usuarioDAO.eliminar(new Usuario(8));
         List<Usuario> usuarios = usuarioDAO.listarUsuarios();
