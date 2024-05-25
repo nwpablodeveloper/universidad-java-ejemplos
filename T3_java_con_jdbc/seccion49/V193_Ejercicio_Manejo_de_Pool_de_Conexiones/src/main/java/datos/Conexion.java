0b9f4c64d78ec060e3cb4b6e2d367612614211bd
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-25 18:53:12 +0200

    Cración de un Pool de Conexiones

---
+++ T3_java_con_jdbc/seccion49/V193_Ejercicio_Manejo_de_Pool_de_Conexiones/src/main/java/datos/Conexion.java
@@ -0,0 +1,47 @@
+package datos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class Conexion {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String PASSWORD = "admin";
+    
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(PASSWORD);
+        
+        // Cantidad del Pool de Conexiones
+        ds.setInitialSize(5);
+        return ds;
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        return getDataSource().getConnection();
+    }
+    
+    public static void close(ResultSet rs) throws SQLException{
+        rs.close();
+    }
+    
+    public static void close(Statement smtm) throws SQLException{
+        smtm.close();
+    }
+    
+    public static void close(PreparedStatement smtm) throws SQLException{
+        smtm.close();
+    }
+    
+    public static void close(Connection conn) throws SQLException{
+        conn.close();
+    }
+}
---
+++ T3_java_con_jdbc/seccion49/V193_Ejercicio_Manejo_de_Pool_de_Conexiones/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,71 @@
+package test;
+
+import datos.Conexion;
+import datos.PersonaDAO;
+import datos.PersonaDaoJDBC;
+import domain.PersonaDTO;
+import java.sql.*;
+import java.util.List;
+
+public class TestManejoPersonas {
+
+    public static void main(String[] args) {
+
+        Connection conexion = null;
+
+        try {
+            
+            // Conexión a la DB
+            conexion = Conexion.getConnection();
+            if( conexion.getAutoCommit() ){
+                conexion.setAutoCommit(false);
+            }
+            
+            // 
+            PersonaDAO personaDAO = new PersonaDaoJDBC(conexion);
+            
+            // Cración objeto Persona
+            PersonaDTO persona1 = new PersonaDTO();
+            persona1.setNombre("Pablo");
+            persona1.setApellido("Veiga");
+            persona1.setEmail("pablo@gmail.com");
+            persona1.setTelefono("613826756");
+            
+            // Preparando para insertar el objecto creado
+            personaDAO.insertar(persona1);
+            
+            // Cración para actualizar un objeto Persona
+            persona1 = new PersonaDTO();
+            persona1.setIdPersona(2);
+            persona1.setNombre("Pablo Andres");
+            persona1.setApellido("Veiga");
+            
+            // Preparar para actualizar
+            personaDAO.actualizar(persona1);
+            
+            // Traer la lista de Personas de la DB
+            List<PersonaDTO> personas = personaDAO.seleccionar();
+            
+            // Mostrar lista de personas
+            personas.forEach( persona -> {
+                System.out.println(persona);
+            });
+            
+            // Ejecutar todas las querys preparadas
+            conexion.commit();
+            System.out.println("Se realizo un commit de la transacción");
+            
+            
+        } catch (SQLException ex) {
+            try {
+                ex.printStackTrace(System.out);
+                System.out.println("Entramos al RollBack");
+                conexion.rollback();
+            } catch (SQLException ex1) {
+                System.out.println("Error en el RollBack");
+                ex1.printStackTrace(System.out);
+            }
+        }
+
+    }
+}
