1d50c62f1858b510989bf6a1b7449cfc21cd9234
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-25 21:01:12 +0200

    Mejora Metodo getDataSource

--- T3_java_con_jdbc/seccion49/V193_Ejercicio_Manejo_de_Pool_de_Conexiones/src/main/java/datos/Conexion.java
+++ T3_java_con_jdbc/seccion49/V193_Ejercicio_Manejo_de_Pool_de_Conexiones/src/main/java/datos/Conexion.java
@@ -13,35 +13,38 @@ public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String PASSWORD = "admin";
-    
-    public static DataSource getDataSource(){
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(PASSWORD);
-        
-        // Cantidad del Pool de Conexiones
-        ds.setInitialSize(5);
-        return ds;
+    private static BasicDataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(PASSWORD);
+
+            // Cantidad del Pool de Conexiones
+            dataSource.setInitialSize(50);
+        }
+        return dataSource;
     }
-    
-    public static Connection getConnection() throws SQLException{
+
+    public static Connection getConnection() throws SQLException {
         return getDataSource().getConnection();
     }
-    
-    public static void close(ResultSet rs) throws SQLException{
+
+    public static void close(ResultSet rs) throws SQLException {
         rs.close();
     }
-    
-    public static void close(Statement smtm) throws SQLException{
+
+    public static void close(Statement smtm) throws SQLException {
         smtm.close();
     }
-    
-    public static void close(PreparedStatement smtm) throws SQLException{
+
+    public static void close(PreparedStatement smtm) throws SQLException {
         smtm.close();
     }
-    
-    public static void close(Connection conn) throws SQLException{
+
+    public static void close(Connection conn) throws SQLException {
         conn.close();
     }
 }
