5865b5e011b45c76c20ac998b4021ee549a68fa5
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-28 20:58:24 +0200

    Hola Mundo Web Con Servelets

---
+++ T4_Servlets_y_JSPs/seccion51/V199_Ejercicio_Hola_Mundo_con_Servelets/src/main/java/mx/com/gm/V199_Ejercicio_Hola_Mundo_con_Servelets/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mx.com.gm.V199_Ejercicio_Hola_Mundo_con_Servelets;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
