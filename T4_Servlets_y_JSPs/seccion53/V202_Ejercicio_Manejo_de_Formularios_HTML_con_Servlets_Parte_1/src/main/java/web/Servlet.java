d909e3817ac1b05bef82ee2effc21296ffb8dcb5
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-30 22:23:40 +0200

    Estructura para manejo de formularios HTML

---
+++ T4_Servlets_y_JSPs/seccion53/V202_Ejercicio_Manejo_de_Formularios_HTML_con_Servlets_Parte_1/src/main/java/web/Servlet.java
@@ -0,0 +1,10 @@
+package web;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet{
+    
+    
+}
