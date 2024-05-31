2d05164862c330de7046e9a3eba68f728b97deb4
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-05-31 22:45:28 +0200

    Declarando estilos en el CSS

---
+++ T4_Servlets_y_JSPs/seccion53/V204_Ejercicio_Manejo_de_Formularios_HTML_con_Servlets_Parte_3/src/main/java/web/Servlet.java
@@ -0,0 +1,10 @@
+package web;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/Servslet")
+public class Servlet extends HttpServlet{
+    
+    
+}
